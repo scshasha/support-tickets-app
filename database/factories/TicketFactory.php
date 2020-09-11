cc88e8e62908585f5ae7b62ac1fe1d3868e20591
Christopher Shasha <shasha.christopher@gmail.com>
2020-09-11 09:14:16 +0200

    Added ticket factory

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -1,14 +1,21 @@
 <?php
+
+use App\User;
 use App\Ticket;
 use App\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Ticket::class, function (Faker $faker) {
+
+    // @TODO: user_id must change to asignee_id or agent_id or asigned_to_id
+
+    // User::all()->random()->id
     return [
         "title" => $faker->sentence,
         "message" => $faker->paragraph,
         "priority" => "low",
         "status" => "Open",
+        "user_id" => $faker->numberBetween(2, 5),
         "ticket_id" => strtoupper(str_random(15)),
         "category_id" => Category::where('name', 'Uncategorized')->firstOrFail()->id,
         "author_name" => $faker->name,
